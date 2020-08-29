e64e5e796e238d226f32b2268f0540ebc39cd023
Sijin Woo <sijin.woo@utexas.edu>
2020-08-28 22:27:27 -0700

    Adds task prototypes

---
+++ Apps/Src/Tasks.c
@@ -0,0 +1,46 @@
+#include "common.h"
+
+void Task_FaultState(void *p_arg) {
+
+}
+
+void Task_CriticalState(void *p_arg) {
+
+}
+
+void Task_PetWDog(void *p_arg) {
+
+}
+
+void Task_VoltTempMonitor(void *p_arg) {
+
+    while(1) {
+        // Update Voltage Measurements
+        
+        // Update
+    }
+}
+
+void Task_AmperesMonitor(void *p_arg) {
+
+}
+
+void Task_CANBus(void *p_arg) {
+
+}
+
+void Task_LogInfo(void *p_arg) {
+
+}
+
+void Task_CLI(void *p_arg) {
+
+}
+
+void Task_BLE(void *p_arg) {
+
+}
+
+void Task_Idle(void *p_arg) {
+
+}
